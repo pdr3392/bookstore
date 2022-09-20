0b0d566d832eaa780d54a7cbae315cedd90a3d3f
pdr3392 <pchenr1que1@gmail.com>
2022-09-20 03:47:20 -0300

    feat: wip45

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = "bookstore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bookstore_dev_db",
+        "USER": "bookstore_dev",
+        "PASSWORD": "bookstore_dev",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
@@ -142,7 +142,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-sycypm_*o2g*eez5cyd1dst&5thgp*+z0^l7kl-v(#s*l5xsme"
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
